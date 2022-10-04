9d7393d9fb6d176dfa85409f4f2d5a2f3e308e9d
freesky <freesky_zh@hotmail.com>
2022-10-04 11:47:09 +0800

    add a new line

--- src/main/java/com/integration/controller/PayloadController.java
+++ src/main/java/com/integration/controller/PayloadController.java
@@ -17,6 +17,7 @@ public class PayloadController {
     @PostMapping("/push")
     public Object push(@RequestBody Object payload) {
         log.info("-- push: " + payload.getClass());
+
         return payload;
     }
 }
