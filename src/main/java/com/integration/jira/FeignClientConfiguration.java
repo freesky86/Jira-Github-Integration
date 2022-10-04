187aa62563f5b1d3b9019e0c2f556fbae23ea70f
freesky <freesky_zh@hotmail.com>
2022-10-04 19:43:19 +0800

    add jira configuration

--- src/main/java/com/integration/jira/FeignClientConfiguration.java
+++ src/main/java/com/integration/jira/FeignClientConfiguration.java
@@ -1,13 +1,24 @@
 package com.integration.jira;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class FeignClientConfiguration {
+
+    @Value("${jira.username}")
+    private String username;
+
+    @Value("${jira.password}")
+    private String password;
+
+
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        return new BasicAuthRequestInterceptor("zhwy79@gmail.com", "wyjizihMepM2QtGfqGVY0A95");
+        return new BasicAuthRequestInterceptor(username, password);
     }
 }
--- src/main/java/com/integration/jira/JiraFeignClient.java
+++ src/main/java/com/integration/jira/JiraFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="jira-rest-api", url="https://freeskymax.atlassian.net/rest/api/2/issue", configuration = FeignClientConfiguration.class)
+@FeignClient(name="jira-rest-api", url="${jira.server}", configuration = FeignClientConfiguration.class)
 public interface JiraFeignClient {
     @PostMapping("/{jiraNo}/comment")
     Object addComments(@RequestBody JiraVo comments, @PathVariable String jiraNo);
