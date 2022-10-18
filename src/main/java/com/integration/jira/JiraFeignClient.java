17b35657a6c3d86aa6d5497415f7056c7fddfe6f
freesky <freesky_zh@hotmail.com>
2022-10-18 20:29:28 +0800

    MOB-2 add class comments

--- src/main/java/com/integration/jira/JiraFeignClient.java
+++ src/main/java/com/integration/jira/JiraFeignClient.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/**
+ * Jira client
+ */
 @FeignClient(name="jira-rest-api", url="${jira.server}", configuration = FeignClientConfiguration.class)
 public interface JiraFeignClient {
     @PostMapping("/{jiraNo}/comment")
--- src/main/java/com/integration/jira/JiraVo.java
+++ src/main/java/com/integration/jira/JiraVo.java
@@ -2,6 +2,9 @@ package com.integration.jira;
 
 import lombok.Data;
 
+/**
+ * Jira request
+ */
 @Data
 public class JiraVo {
     private String body;
